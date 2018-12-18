ffb7d69274c98d680f97b3e7c6176ad881795888
VladyaRazumist <45969614+VladyaRazumist@users.noreply.github.com>
2018-12-18 19:56:58 +0700

    Add files via upload

---
+++ Group.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package studentmodel;
+
+/**
+ *
+ * @author леха
+ */
+public class Group {
+    private final int numberOfStudents; // types of students actually
+    private final Student [] students;
+    
+    
+    public Group(int nofS,double [] p, double [] q, int [] w){   
+    this.numberOfStudents=nofS; 
+    this.students = new Student[numberOfStudents]; // array of student`s types
+        for (int i=0;i<numberOfStudents;i++) {
+            students[i]=new Student(w[i],p[i],q[i],i+1);
+        }
+        
+    }
+        
+     
+        public Student getStudent(int i) {
+            return this.students[i];
+        }
+        public void PrintGroup () {
+            for (Student S:students)
+            {
+                int i=1;
+                System.out.println("N="+S.getNum() + "  " + "p="+S.getP() +"  "+"q="+S.getQ()+"  "+"w="+S.getWeight());
+                i++;
+            } 
+        }
+      
+                
+        }
+
