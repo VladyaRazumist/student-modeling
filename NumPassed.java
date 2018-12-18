eacd2eefbc875205e8cbf758967ea1ebadd2b2ab
VladyaRazumist <45969614+VladyaRazumist@users.noreply.github.com>
2018-12-18 20:36:37 +0700

    Update NumPassed.java

--- NumPassed.java
+++ NumPassed.java
@@ -66,47 +66,49 @@ public class NumPassed {
         this.N=P*S.getWeight();
         return N;
 }
-  public StartFinish getNeighbour (int t1, int t2)throws InvalidDistanceException {
+   public StartFinish getNeighbour (int t1, int t2)throws InvalidDistanceException {
      double random1=Math.random();
-     double random2=Math.random();
+    StartFinish sf=new StartFinish(t1,t2);
      int max=T.getT2();
      int min=T.getT1();
      if ((t2-t1)>max || (t2-t1)<min){
          throw new InvalidDistanceException("Invalid distance");
      }
-      if (random1<0.5){ // move t1
-          if (random2<0.5){ // move t1 left
-              if (t1!=0 && (t2-t1)!=max) {
-                  t1=t1-1;
-                  if (((t2-t1)==max || t1==0)&& (t2-t1)!=min ) {
-                      t1=t1+1;          // or right 
-                  } 
-              }
+      if (random1<=0.25){  // move t1 left
+          if (t1==0 || (t2-t1)==max ){
+              return sf=getNeighbour(t1,t2);
+     
+      } else {
+              t1=t1-1;
+             return  sf = new StartFinish(t1,t2);
           }
-          if (random2>=0.5){ // move t1 right
-              if((t2-t1)!=min){
-                  t1=t1+1;
-                  if ((t2-t1)==min && t1!=0 && (t2-t1)!=max ){
-                      t1=t1-1; // or left
-                  }
-              }
+      
+  }
+      if(random1>0.25 && random1<=0.5){ // move t1 right
+          if ((t2-t1)==min) {
+              return sf=getNeighbour(t1,t2);
+          } else {
+              t1=t1+1;
+               return  sf = new StartFinish(t1,t2);
           }
       }
-      if (random1>=0.5) {  // move t2
-           if (random2>=0.5 && t2!=S.getlenL() && (t2-t1)!=max){ // move t2 right   
-               t2=t2+1;
-               if ((t2==S.getlenL() || (t2-t1)==max)&& (t2-t1)!=min) { // or left
-                   t2=t2-1;
-               }
-           }    
-           if (random2<0.5 && (t2-t1)!=min){ // move t2 left
-               t2=t2-1; 
-               if ((t2-t1)==min && t2!=S.getlenL() && (t2-t1)!=max){
-                   t2=t2+1;
-               }
-           } 
-  }
-      StartFinish sf=new StartFinish(t1,t2);
+      if (random1>0.5 && random1<=0.75){ // move t2 left
+          if ((t2-t1)==min){
+              return sf=getNeighbour(t1,t2);
+          } else {
+              t2=t2-1;
+              return  sf = new StartFinish(t1,t2);
+          }
+      }
+      if (random1>0.75 ){  // move t2 right
+          if ((t2-t1)==max || t2==S.getlenL()-1) {
+              return sf=getNeighbour(t1,t2);
+          } else {
+              t2=t2+1;
+              return sf=getNeighbour(t1,t2);
+          }
+      }
+      
       return sf;
   }
     
